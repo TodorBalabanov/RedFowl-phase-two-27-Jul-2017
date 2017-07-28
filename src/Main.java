64f104f4ee9b43c5f7e9c52beef8e623a43722f2
Todor Balabanov <todorbalabanov@MACMINI.local>
2017-07-28 18:30:58 +0300

    Simulator should be correct now.

--- src/Main.java
+++ src/Main.java
@@ -30,23 +30,31 @@ class Ball implements Comparable<Ball> {
 public class Main {
 
 	private static final Ball BALLS[] = { new Ball(1, 0), new Ball(2, 1), new Ball(3, 2), new Ball(4, 3),
-			new Ball(5, 4), new Ball(6, 5), new Ball(7, 6), new Ball(8, 7), new Ball(9, 8), new Ball(10, 0),
-			new Ball(11, 1), new Ball(12, 2), new Ball(13, 3), new Ball(14, 4), new Ball(15, 5), new Ball(16, 6),
-			new Ball(17, 7), new Ball(18, 8), new Ball(19, 0), new Ball(20, 1), new Ball(21, 2), new Ball(22, 3),
-			new Ball(23, 4), new Ball(24, 5), new Ball(25, 6), new Ball(26, 7), new Ball(27, 8), new Ball(28, 0),
-			new Ball(29, 1), new Ball(30, 2), new Ball(31, 3), new Ball(32, 4), new Ball(33, 5), new Ball(34, 6),
-			new Ball(35, 7), new Ball(36, 8), new Ball(37, 0), new Ball(38, 1), new Ball(39, 2), new Ball(40, 3),
-			new Ball(41, 4), new Ball(42, 5), new Ball(43, 6), new Ball(44, 7), new Ball(45, 8), new Ball(46, 0),
-			new Ball(47, 1), new Ball(48, 2), new Ball(49, 3), new Ball(50, 4), new Ball(51, 5), new Ball(52, 6),
-			new Ball(53, 7), new Ball(54, 8), new Ball(55, 0), new Ball(56, 1), new Ball(57, 2), new Ball(58, 3),
-			new Ball(59, 4), new Ball(60, 5), new Ball(61, 6), new Ball(62, 7), new Ball(63, 8), new Ball(64, 0),
-			new Ball(65, 1), new Ball(66, 2), new Ball(67, 3), new Ball(68, 4), new Ball(69, 5), new Ball(70, 6),
-			new Ball(71, 7), new Ball(72, 8), new Ball(73, 0), new Ball(74, 1), new Ball(75, 2), new Ball(76, 3),
-			new Ball(77, 4), new Ball(78, 5), new Ball(79, 6), new Ball(80, 7), new Ball(81, 8), new Ball(82, 0),
-			new Ball(83, 1), new Ball(84, 2), new Ball(85, 3), new Ball(86, 4), new Ball(87, 5), new Ball(88, 6),
-			new Ball(89, 7), new Ball(90, 8),
+			new Ball(5, 4), new Ball(6, 5), new Ball(7, 6), new Ball(8, 7), new Ball(9, 8), new Ball(10, 9),
 
-	};
+			new Ball(11, 0), new Ball(12, 1), new Ball(13, 2), new Ball(14, 3), new Ball(15, 4), new Ball(16, 5),
+			new Ball(17, 6), new Ball(18, 7), new Ball(19, 8), new Ball(20, 9),
+
+			new Ball(21, 0), new Ball(22, 1), new Ball(23, 2), new Ball(24, 3), new Ball(25, 4), new Ball(26, 5),
+			new Ball(27, 6), new Ball(28, 7), new Ball(29, 8), new Ball(30, 9),
+
+			new Ball(31, 0), new Ball(32, 1), new Ball(33, 2), new Ball(34, 3), new Ball(35, 4), new Ball(36, 5),
+			new Ball(37, 6), new Ball(38, 7), new Ball(39, 8), new Ball(40, 9),
+
+			new Ball(41, 0), new Ball(42, 1), new Ball(43, 2), new Ball(44, 3), new Ball(45, 4), new Ball(46, 5),
+			new Ball(47, 6), new Ball(48, 7), new Ball(49, 8), new Ball(50, 9),
+
+			new Ball(51, 0), new Ball(52, 1), new Ball(53, 2), new Ball(54, 3), new Ball(55, 4), new Ball(56, 5),
+			new Ball(57, 6), new Ball(58, 7), new Ball(59, 8), new Ball(60, 9),
+
+			new Ball(61, 0), new Ball(62, 1), new Ball(63, 2), new Ball(64, 3), new Ball(65, 4), new Ball(66, 5),
+			new Ball(67, 6), new Ball(68, 7), new Ball(69, 8), new Ball(70, 9),
+
+			new Ball(71, 0), new Ball(72, 1), new Ball(73, 2), new Ball(74, 3), new Ball(75, 4), new Ball(76, 5),
+			new Ball(77, 6), new Ball(78, 7), new Ball(79, 8), new Ball(80, 9),
+
+			new Ball(81, 0), new Ball(82, 1), new Ball(83, 2), new Ball(84, 3), new Ball(85, 4), new Ball(86, 5),
+			new Ball(87, 6), new Ball(88, 7), new Ball(89, 8), new Ball(90, 9), };
 
 	private static final Random PRNG = new Random();
 
@@ -89,24 +97,77 @@ public class Main {
 			max = 0;
 			count = 0;
 
-			for (int k = 0; k < drawn.length; k++) {
+			/*
+			 * Check all drawn balls.
+			 */
+			loop: for (int k = 0; k < drawn.length; k++) {
+
+				/*
+				 * Check all balls marked in the ticket.
+				 */
 				for (int j = 0; j < tickets[i].length; j++) {
+
+					/*
+					 * When ball is marked in the ticket.
+					 */
 					if (tickets[i][j] == drawn[k]) {
 						count++;
 
+						/*
+						 * Mark the position of the latest ball guess.
+						 */
 						if (max < k) {
 							max = k;
 						}
+
+						/*
+						 * Six balls are known.
+						 */
+						if (count >= 6) {
+							break loop;
+						}
+
+						/*
+						 * If the number is found in the ticket there is no need
+						 * to search the rest of the ticket.
+						 */
+						break;
 					}
 				}
 			}
 
-			if (count == tickets[i].length) {
+			if (count >= 6) {
+				if (max == 4) {
+					System.err.println(count);
+					System.err.println(max);
+					System.err.println(Arrays.toString(drawn));
+					System.err.println(Arrays.toString(tickets[i]));
+					System.exit(0);
+				}
 				counters[i][max + 1]++;
 			} else {
 				counters[i][0]++;
 			}
+
 		}
+
+		/*
+		 * Count colors.
+		 */
+		int colors[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		for (int k = 0; k < drawn.length; k++) {
+			colors[drawn[k].color]++;
+		}
+		int sum = 0;
+		for (int i = 0; i < colors.length; i++) {
+			/*
+			 * Count if more than six balls from the same color are drawn.
+			 */
+			if (colors[i] >= 6) {
+				sum++;
+			}
+		}
+		counters[5][sum]++;
 	}
 
 	private static void simulate(long runs) {
@@ -140,7 +201,9 @@ public class Main {
 				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 						0, 0 },
 				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-						0, 0 }, };
+						0, 0 },
+
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
 
 		/*
 		 * Simulation.
@@ -198,7 +261,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		simulate(10000000000L);
+		simulate(100000000L);
 	}
 
 }
